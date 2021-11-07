721b5097e8fdc46f4354c71bfdebc1842765d8cf
khendrikse <30577427+khendrikse@users.noreply.github.com>
2021-11-07 11:10:25 +0100

    Add more logic and some styling

---
+++ src/preload.js
@@ -0,0 +1,32 @@
+const { ipcRenderer, contextBridge } = require('electron');
+const {
+  parse,
+  milliseconds,
+  getMinutes,
+  getSeconds,
+  getHours
+} = require('date-fns');
+
+function init() {
+  contextBridge.exposeInMainWorld('electron', {
+    getBreakContent: elementId =>
+      ipcRenderer.invoke('get-body').then(result => {
+        document.getElementById(elementId).innerHTML = result;
+      }),
+    openBreakWindow: currentTimer =>
+      ipcRenderer.send('open-break', currentTimer),
+    closeBreakWindow: () => ipcRenderer.send('close-break')
+  });
+  contextBridge.exposeInMainWorld('isElectron', true);
+  contextBridge.exposeInMainWorld('dateFns', {
+    convertToMilliseconds: timeString => {
+      const dateObject = parse(timeString, 'HH:mm:ss', new Date());
+      const hours = getHours(dateObject);
+      const minutes = getMinutes(dateObject);
+      const seconds = getSeconds(dateObject);
+      return milliseconds({ hours, minutes, seconds });
+    }
+  });
+}
+
+init();
---
+++ src/windows/menu.js
@@ -0,0 +1,22 @@
+const { BrowserWindow } = require('electron');
+const path = require('path');
+
+const menuWindow = () => {
+  const window = new BrowserWindow({
+    width: 500,
+    height: 850,
+    frame: false,
+    // transparent: true,
+    hasShadow: false,
+    resizable: false,
+    webPreferences: {
+      preload: path.join(__dirname, '../preload.js')
+    }
+  });
+
+  window.webContents.openDevTools();
+  window.loadURL(`file://${__dirname}/../../html/menuWindow.html`);
+  return window;
+};
+
+module.exports = menuWindow;
